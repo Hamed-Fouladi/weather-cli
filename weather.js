539d854e47185c5f3231ee8eee80ef8d796f9d60
Hamed Fouladi <hamed.fouladi27@gmail.com>
2022-04-14 15:07:09 +0300

    application termination

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather} from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon} from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -17,18 +17,47 @@ const saveToken = async (token) => {
     }
 }
 
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('No city passed!');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('City saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon));
+    } catch (e) {
+        if (e?.response?.status === 404) {
+            printError('City is incorrect');
+        }else if (e?.response?.status === 401) {
+            printError('Token is incorrect');
+        } else {
+            printError(e.message);
+        }
+    }
+};
+
 const initCLI = () => {
     const args = getArgs(process.argv)
     if (args.h) {
-        printHelp();
+        return printHelp();
     }
     if (args.s) {
+        return saveCity(args.s);
     }
     if (args.t) {
         return saveToken(args.t);
     }
-    getWeather('Kharkov');
-    // Output weather
+    return getForcast();
 };
 
 initCLI();
