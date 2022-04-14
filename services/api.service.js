627cef1ae31ca1c18a965fc87eb7f6790de1a226
Hamed Fouladi <hamed.fouladi27@gmail.com>
2022-04-14 10:48:56 +0300

    Interaction with API v1.0

--- services/api.service.js
+++ services/api.service.js
@@ -1,26 +1,19 @@
-import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+import axios from 'axios';
 
 const getWeather = async (city) => {
     const token = await getKeyValue(TOKEN_DICTIONARY.token);
     if (!token) {
         throw new Error('The API key is not set, set it through the command -t [API_KEY]');
     }
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-    url.searchParams.append('q', city);
-    url.searchParams.append('appid', token);
-    url.searchParams.append('units', 'metric');
-    https.get(url, (response) => {
-        let res = '';
-        response.on('data', (chunk) => {
-            res += chunk;
-        });
-
-        response.on('end', () => {
-            console.log(res);
-        });
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            units: 'metric'
+        }
     });
+    return data;
 };
 
 export { getWeather };
